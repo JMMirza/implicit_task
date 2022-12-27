7d63ca0f2442153eea3fb9487139b97e8742d180
jmmirza <janmuhammadmirza@gmail.com>
2022-12-27 17:09:17 +0500

    changes

--- controller/user/signup.js
+++ controller/user/signup.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASSWORD,
     },
 });
-module.exports = async(req, res) => {
+module.exports = async (req, res) => {
     try {
         if (req.body.password !== req.body.cpassword) return res.status(400).send({ message: 'Password doesnot match' });
         const salt = await bcrypt.genSalt(10)
@@ -29,12 +29,12 @@ module.exports = async(req, res) => {
         const url = `http://localhost:3000/api/user/verify/${token}`;
 
         await transporter.sendMail({
-                from: "edummy304@gmail.com",
-                to: user.email,
-                subject: 'Verify Account',
-                html: `Click <a href = '${url}'>here</a> to confirm your email.`
-            })
-            // const result = await user.save()
+            from: "edummy304@gmail.com",
+            to: user.email,
+            subject: 'Verify Account',
+            html: `Thanks for registering. Please click <a href = '${url}'>here</a> to confirm your email.`
+        })
+        // const result = await user.save()
         return res.status(201).json({
             message: `Sent a verification email to ${user.email}.`,
             id: `${user._id}`,
